7d3b234834457fb8cdfd5f2b7e6a8fa464278e7b
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-01-10 21:25:43 +0200

    update version

--- chat/chat/__init__.py
+++ chat/chat/__init__.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, HTTPException
 
 redis_host = os.environ["REDIS_HOST"] if "REDIS_HOST" in os.environ else "localhost"
 redis_pass = os.environ["REDIS_PASS"] if "REDIS_PASS" in os.environ else None
-vers = "v0.2"
+vers = "v0.3"
 app = FastAPI()
 r = redis.Redis(host=redis_host, port=6379, password=redis_pass, db=0)
 
@@ -16,7 +16,7 @@ def read_root():
         r.set("foo", "baz")
         result = r.get("foo")
         print(result)
-        return str(result) + "\nVersion: " + vers
+        return {"result": result, "version": vers}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
